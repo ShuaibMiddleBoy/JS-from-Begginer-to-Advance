793db70ba8263c09a207213466d9f45ac626ae3a
Shuaib Khan <shuaib.middle@gmail.com>
2023-06-24 12:26:56 +0500

    typeof operators and convert number to string and vice versa

---
+++ script5.js
@@ -0,0 +1,38 @@
+// typeof operators 
+
+// data types (primitive data types)
+// String 
+// number 
+// undefined 
+// Boolean
+// null 
+// BigInt 
+// Symbol 
+
+
+let name = "Shuaib Khan";
+let age = 22;
+
+// console.log(typeof name);
+// console.log(typeof age);
+
+
+
+// converting number to string
+// console.log(typeof (age+""));
+// age = age + "";
+// console.log(typeof age);
+// let number = 23;
+// let toString = String(number)
+// console.log(typeof toString);
+
+
+
+// converting string to number 
+
+// let number = +"23";
+// console.log(typeof number);
+
+let str = "23";
+let toNum = Number(str); 
+console.log(typeof toNum);
