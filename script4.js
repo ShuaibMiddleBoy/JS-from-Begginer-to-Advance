8f9539a858b1b18c4938bc9d32663d46461dbd9e
Shuaib Khan <shuaib.middle@gmail.com>
2023-06-24 11:25:22 +0500

    Done Strings

---
+++ script4.js
@@ -0,0 +1,49 @@
+// string indexing 
+
+let name = "Shuaib Khan";
+
+// S h u a i b   K h a  n 
+// 0 1 2 3 4 5 6 7 8 9 10 
+
+// console.log(name[7]);
+
+// console.log(name.length);
+
+// console.log(name[name.length-1]);  last Index
+
+
+// string methods 
+
+
+// trim()
+
+// let firstName = "    Shuaib Khan    ";
+// console.log(firstName.length);
+// let newString = firstName.trim();
+// firstName = firstName.trim();
+// console.log(firstName.length);
+
+
+
+
+// toUpperCase()
+
+// let myName = "Shuaib Khan";
+// console.log(myName);
+// myName = myName.toUpperCase();
+// console.log(myName);
+
+
+// toLowerCase()
+
+// let myName = "Zia ur Rehman";
+// console.log(myName);
+// myName = myName.toLowerCase();
+// console.log(myName); 
+
+
+// slice()
+let fullName = "Shuaib Khan";
+// let particularWords = fullName.slice(0,5);
+let particularWords = fullName.slice(1);
+console.log(particularWords);
