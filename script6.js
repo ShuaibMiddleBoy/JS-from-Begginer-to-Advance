a76c1e656fc145edaec80a4b5df5b821e920b826
Shuaib Khan <shuaib.middle@gmail.com>
2023-06-24 23:15:40 +0500

    ternary operatory

---
+++ script6.js
@@ -0,0 +1,17 @@
+// let name;
+// console.log(typeof name, name);
+
+
+// const bug = null;
+// console.log(typeof bug);
+
+
+// const num = BigInt(2323);
+// console.log(typeof num);
+
+const value = "true";
+
+console.log(value == "true" ? "Okay" : "Sorry");
+
+// const result = value === "true" ? "Awesome" : "Sorry";
+// console.log(result);
