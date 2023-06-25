3461fa8f8da2ac673e79f7bf5e9dd94705734ff8
Shuaib Khan <shuaib.middle@gmail.com>
2023-06-25 14:54:53 +0500

    about array and object

---
+++ script9.js
@@ -0,0 +1,79 @@
+// let num1 = 9;
+// num1++;
+// let num2 = num1;
+// console.log(num1);
+// console.log(num2);
+
+
+// let arr1 = ["a","b"];
+// let arr2 = arr1;
+// arr1.push("c");
+// console.log(arr1);
+// console.log(arr2);
+
+
+const arr = ["apple", "mango", "banana", "graphs"];
+// arr.push("water melon")
+// for(let i=0; i<arr.length; i++){
+//     console.log(arr[i].toUpperCase());
+// }
+
+// let i = 0;
+// while(i<arr.length){
+//     console.log(arr[i].toLocaleUpperCase());
+//     i++;
+// }
+
+
+// const [a, b, ...newArr] = arr;
+// console.log(a,b);
+// console.log(newArr);
+
+// let key = "email";
+// const obj = {
+//     name : "Shuaib",
+//     age : 23
+// }
+
+// obj[key] = "skhan.csit@gmail.com";
+
+// for(let o in obj){
+//     console.log(o +" : "+obj[o]);
+// }
+
+// const keys = Object.keys(obj);
+// for(let key of keys){
+//     console.log(obj[key]);
+// }
+
+
+let key_1 = "key1";
+let key_2 = "key2";
+
+let value_1 = "value1";
+let value_2 = "value2";
+
+// const obj = {};
+// obj[key_1] = value_1;
+// obj[key_2] = value_2;
+// console.log(obj);
+
+
+// const obj = {
+//     [key_1] : value_1,
+//     [key_2] : value_2
+// };
+// console.log(obj);
+
+// const obj = {..."abcdefghijklmnopqrstuvwxyz"};
+// console.log(obj);
+
+const obj = {
+    name : "Shuaib",
+    age : 22,
+    gender : "male",
+    qualification : "Web developer"
+}
+
+const {name, age:a, ...rest} = obj;
+console.log(rest);
