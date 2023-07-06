95dce48bbc6016f44b6c64fd61ebee9252049287
Shuaib Khan <shuaib.middle@gmail.com>
2023-07-07 00:00:55 +0500

    Array fill method

---
+++ fillMethod.js
@@ -0,0 +1,10 @@
+// fill method *******************************
+// value,   start,   end
+
+
+// const arr = new Array(10).fill(-1);
+// console.log(arr);
+
+const numbers = [1,2,3,4,5,6,7,8,9];
+const result = numbers.fill(0,2,5);
+console.log(result);
