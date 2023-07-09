8dbd83f258261bcf4e28f87789ee9cdba6d93a53
Shuaib Khan <shuaib.middle@gmail.com>
2023-07-10 00:38:09 +0500

    variables in js

---
+++ variables.js
@@ -0,0 +1,47 @@
+"use strict";
+
+// Variables in Javascript
+
+// variables can store some information
+// we can use that information later
+// we can change also that information later
+
+// declare a variables
+var firstName = "Shuaib";
+var lastName = "Khan";
+
+// use a variables
+console.log(firstName, lastName);
+
+// change a variable value
+lastName = "Muhammad";
+
+console.log(firstName, lastName);
+
+// rules for naming variables
+
+// you cannot start with number
+
+// example: 1name = "shuaib"; (invalid)
+//          name1 = "shuaib"; (valid)
+
+// in special characters use can use only dollar symbol ($) or underScore (_)
+
+// example: $firstName = "Shuaib"; (valid)
+//          first$name = "Shuaib"; (valid)
+
+//          first_name = "Asad"; (valid)
+//         _firstName = "shuaib"; (valid)
+
+
+// use cannot use spaces 
+// first name = "Shuaib" (invalid)
+
+
+
+// first_name (snake case)
+// firstName (camel case)
+
+// convention
+// start with small letter and use camelCase
+
