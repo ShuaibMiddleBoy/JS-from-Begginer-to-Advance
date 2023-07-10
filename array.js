02dd5dda28580c5a165b3e4a1e2c056f24a55312
Shuaib Khan <shuaib.middle@gmail.com>
2023-07-10 09:16:31 +0500

    Array in js

---
+++ array.js
@@ -0,0 +1,26 @@
+// let num1 = 9;
+// num1++;
+// let num2 = num1;
+// num1++;
+// console.log(num1);
+// console.log(num2);
+
+
+// let arr1 = ["a","b"];
+// let arr2 = arr1;
+// arr1.push("c");
+// console.log(arr1);
+// console.log(arr2);
+
+
+// const arr = ["apple", "mango", "banana", "graphs"];
+// arr.push("water melon")
+// for(let i=0; i<arr.length; i++){
+//     console.log(arr[i].toUpperCase());
+// }
+
+// let i = 0;
+// while(i<arr.length){
+//     console.log(arr[i].toLocaleUpperCase());
+//     i++;
+// }
